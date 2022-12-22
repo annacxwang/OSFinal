e48199e0c824df2a6cdc91c4db5f12fdcac8e84b
Chunxiao Wang <36002539+annacxwang@users.noreply.github.com>
2022-12-22 07:03:05 -0500

    Add files via upload

--- run.c
+++ run.c
@@ -4,7 +4,9 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/time.h>
 #include <unistd.h>
+#include <math.h>
 
 int isNumber(char in[]){
 
@@ -24,7 +26,12 @@ unsigned int xorbuf(unsigned int *buffer, int size) {
     return result;
 }
 
+double now(){
+    struct timeval tv;
+    gettimeofday(&tv,0);
+    return tv.tv_usec/1000.0 + tv.tv_sec*1000.0;
 
+}
 int main(int argc,char * argv[])
 {
    /* printf function displays the content that is
@@ -39,12 +46,14 @@ int main(int argc,char * argv[])
     	printf("Check usage : ./run <filename> [-r|-w] <block_size> <block_count>\n");
     	return 0;
     }
-    int block_size = atoi(argv[3]);
-    int block_ct = atoi(argv[4]);
-    int buf_size = block_size * block_ct; // file size in bytes
+    long block_size = atoi(argv[3]);
+    long block_ct = atoi(argv[4]);
+    long buf_size = block_size * block_ct; // file size in bytes
     if(buf_size % 4 != 0){
     	buf_size = buf_size - (buf_size %4); 
     }
+    
+    double start,end;
     
     if(strcmp(argv[2],"-r")==0){
     	//printf("1\n");
@@ -54,28 +63,60 @@ int main(int argc,char * argv[])
     	    return 0;
     	}
     	unsigned int* buf= malloc(buf_size);
+    	start = now();
     	long byte_read = read(fd,buf,buf_size);
+    	end = now();
+    	
     	if (byte_read == -1){
     	    printf("Fail to read from: %s\n",argv[1]);
     	    return 0;
     	}
-    	printf("%ld\n",byte_read);
+    	printf("%ld bytes read in %f milliseconds\n",byte_read,end-start);
     	/**
     	for(int i  =0;i<buf_size/sizeof(int);i++){
     	    printf("%d ",buf[i]);
     	}
     	**/
-    	printf("xor result is %08x\n",xorbuf(buf,buf_size/sizeof(int)));
+    	//printf("xor result is %08x\n",xorbuf(buf,buf_size/sizeof(int)));
     	int SIZE = buf_size/sizeof(int);
     	unsigned int xor1 = xorbuf(buf, SIZE / 2);
     	unsigned int xor2 = xorbuf(buf + SIZE / 2, SIZE / 2);
     	printf("xor result is %08x\n",xor1^xor2);
+    	double MiB = byte_read / pow(2.0,20.0);
+    	double seconds = (end - start) /1000.0;
+    	//printf("%f MiB %f seconds\n",MiB,seconds);
+    	printf("reading speed is %f MiB/s\n",MiB/seconds);
     	close(fd);
     
     }
     else if(strcmp(argv[2],"-w")==0){
-    printf("2\n");
-    
+    	//printf("2\n");
+    	int fd = open(argv[1],O_RDWR|O_CREAT|O_TRUNC,S_IRWXU);
+    	if(fd == -1){
+    	    printf("Fail to open file: %s\n",argv[1]);
+    	    return 0;
+    	}
+    	unsigned int* buf= malloc(buf_size);
+    	srandom(time(NULL));
+    	for(int i = 0; i<buf_size/sizeof(int);i++){
+    	    buf[i] = random();
+    	}
+    	
+    	start = now();
+    	long byte_written = write(fd,buf,buf_size);
+    	end = now();
+    	
+    	if (byte_written == -1){
+    	    printf("Fail to write to: %s\n",argv[1]);
+    	    return 0;
+    	}
+    	printf("%ld bytes written in %f milliseconds\n",byte_written,end-start);
+    	
+    	double MB = byte_written / pow(10.0,6.0);
+    	double seconds = (end - start) /1000.0;
+    	//printf("%f MB %f seconds\n",MB,seconds);
+    	printf("writing speed is %f MB/s\n",MB/seconds);
+    	close(fd);
     }
     else{
     printf("Check usage : ./run <filename> [-r|-w] <block_size> <block_count>\n");
