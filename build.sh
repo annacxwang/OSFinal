7e785c3f982032382ba37b4672e818419c3d2cab
Chunxiao Wang <36002539+annacxwang@users.noreply.github.com>
2022-12-22 22:51:41 -0500

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-do
+
 gcc run.c -o run
 gcc run2.c -o run2
+gcc run3.c -o run3
+gcc run4.c -o run4
+gcc run5.c -o run5
+gcc fast.c -o fast
 ./2.sh
 ./run part3 -w 364000 2000
-gcc run3.c -o run3
 ./3.sh
-gcc run4.c -o run4
 ./4.sh
-gcc run5.c -o run5
 ./5.sh
-gcc fast.c -o fast
-done
